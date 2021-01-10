a0b4fb02693737ef592ac00f9c39eee0cbd3a645
omarrifky <36340225+omarrifky@users.noreply.github.com>
2021-01-10 12:02:10 +0200

    Update DBConnector.java

--- demo/src/main/java/com/example/demo/DBConnector.java
+++ demo/src/main/java/com/example/demo/DBConnector.java
@@ -4,7 +4,7 @@ public class DBConnector {
 	
 	public static void main (String args []) {
 		try {
-			Connection myConn =DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?useSSL=false","root","Tatafifi1997");			
+			Connection myConn =DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?useSSL=false","root","Password");			
 			Statement myStmt = myConn.createStatement();
 			
 			ResultSet myRs = myStmt.executeQuery("select * from Owner");
